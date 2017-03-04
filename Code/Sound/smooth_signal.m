a926fced5e947521d5ee93358cb528dbcc7875f5
deCodeIt <786alijafri786@gmail.com>
2017-03-04 12:59:47 +0530

    setting peak mode to detect envelop

--- Code/Sound/smooth_signal.m
+++ Code/Sound/smooth_signal.m
@@ -1,5 +1,8 @@
 function[x,sY] = smooth_signal(y,num_samples)
 
+    % for entire waveform of 40 secs take num_samples = 5000, and for each
+    % frequency component i.e. 2 secs interval, take num_samples = 1000
+    
     % num_samples = 1000; % samples in one sliding window
     coeff = ones(1,num_samples)/num_samples; %coeff for smoothening
     fDelay = (length(coeff)-1)/2;
@@ -7,7 +10,7 @@ function[x,sY] = smooth_signal(y,num_samples)
     x = 1:size(y,1);
     x = x-fDelay/num_samples;
     
-    [eH,eL] = envelope(y,num_samples,'rms');
+    [eH,~] = envelope(y,num_samples,'peak');
     sY = filter(coeff,1,eH);
 
     % hold on;
