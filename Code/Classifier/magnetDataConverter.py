6e4bc9f00874d8ec74eed9afa4a031334fcf7626
Ali Jafri <shaikhali.jafri@iitrpr.ac.in>
2017-05-07 22:33:37 +0530

    Magnet Data Converted

---
+++ Code/Classifier/magnetDataConverter.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import sys
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import hilbert
+import struct
+
+def getParamsFromDist(dist):
+	'''returns features from a distribution'''
+	mean = max(dist);
+	std = min(dist);
+	r = max(dist) - min(dist);
+	return np.array([mean,std,r]);
+
+class MagnetProcessing():
+
+	def __init__(self):
+		self.dataSet = None; # will contain the dataSet of Sound
+
+	def MagnetReadAndConvert(self):
+		# open(fname,mode) is the Python way of reading files
+		data = np.array([]);
+		with open(self.fileName,"r") as fin: # Read wav file, "r flag" - read, "b flag" - binary 
+			for line in fin:
+				data = [ float(_d) for _d in line.split(',') ];
+				x = data[:-3]; # to leave last 3 columns as they are labels and NOT readings
+				y = data[-3:]; # the data labels
+
+				dataFeatures = getParamsFromDist(x);
+				dataTuple = np.append(dataFeatures,y);
+				if self.dataSet is None:
+					self.dataSet = np.reshape(dataTuple,(-1,dataTuple.shape[0]));
+				else:
+					self.dataSet = np.concatenate(( self.dataSet, np.reshape(dataTuple,(-1,dataTuple.shape[0])) ));
+
+		print(self.dataSet)
+
+	def run(self,fileName):
+		'''Call this function to initiate data generation'''
+		self.fileName = fileName;
+		self.MagnetReadAndConvert();
+
+
+
+
+def main():
+	# try:
+	obj = MagnetProcessing();
+	magnetFile = 'data/magnet_data/magnet_data.txt';
+	obj.run(magnetFile)
+
+	# save the data to a file
+	np.savetxt('magnet_data.txt',obj.dataSet,fmt='%.3f',delimiter=",");
+
+	# except Exception as e:
+	# 	print("{'status':'false','error':'"+str(e).replace("'","\\'")+"'}");
+
+if __name__ == '__main__':
+	main()
