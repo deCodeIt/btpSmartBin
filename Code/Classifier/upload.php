29a0a842667195f54c79fe0c0b31b0964cb3ae06
Ali Jafri <shaikhali.jafri@iitrpr.ac.in>
2017-05-08 15:52:23 +0530

    sample html page to upload the two files

---
+++ Code/Classifier/upload.php
@@ -0,0 +1,6 @@
+<form action="predictor.php" method="post" enctype="multipart/form-data">
+  Send these files:<br />
+  <input name="file1" type="file" /><br />
+  <input name="file2" type="file" /><br />
+  <input type="submit" value="Send files" />
+</form>
